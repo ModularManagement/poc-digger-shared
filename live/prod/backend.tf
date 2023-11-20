6b868b0c261478e258985d57681eadb5925ae09b
ubbeK <urban.nyden@gmail.com>
2023-11-20 14:01:35 +0100

    terragrunt files added

---
+++ live/prod/backend.tf
@@ -0,0 +1,11 @@
+# https://developer.hashicorp.com/terraform/language/settings/backends/s3
+terraform {
+  backend "s3" {
+    bucket         = "infra-poc-gitops-terraform-lab-en1"
+    key            = "poc-gitops-terraform/live/prod/terraform.tfstate"
+    region         = "eu-north-1"
+    profile        = "c10r-lab"
+    dynamodb_table = "infra-poc-gitops-terraform-lab-en1-lock"
+    encrypt        = true
+  }
+}
