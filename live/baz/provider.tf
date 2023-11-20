74cf7ec1942ebcfa271a8bffada6784efcc38a2c
ubbeK <urban.nyden@gmail.com>
2023-11-20 13:35:54 +0100

    gh environments

--- live/baz/provider.tf
+++
@@ -1,5 +0,0 @@
-# AWS provider docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-provider "aws" {
-  region  = "REMOVED"
-  profile = "REMOVED"
-}
--- live/baz/terragrunt.hcl
+++
@@ -1,5 +0,0 @@
-dependency "bar" {
-  config_path = "${get_original_terragrunt_dir()}/../bar"
-
-  skip_outputs = true
-}
