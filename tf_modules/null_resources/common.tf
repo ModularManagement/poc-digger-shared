012434dfc0992359c6dfe988d2f05a9b08d80f75
ubbeK <urban.nyden@gmail.com>
2023-11-24 11:12:42 +0100

    demo

--- tf_modules/null_resources/common.tf
+++ tf_modules/null_resources/common.tf
@@ -2,9 +2,9 @@ resource "null_resource" "common_001" {
 
 }
 
-# resource "null_resource" "common_002" {
+resource "null_resource" "common_002" {
 
-# }
+}
 
 # resource "null_resource" "common_003" {
 
